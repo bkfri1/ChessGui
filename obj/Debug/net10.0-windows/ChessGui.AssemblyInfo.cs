f2d9aa18bff783846eb382f4dd71a95d593b047a
bkfri1 <boaz.kfirgmil@gmail.com>
2026-09-08 18:46:15 +0300

    idk

--- obj/Debug/net10.0-windows/ChessGui.AssemblyInfo.cs
+++ obj/Debug/net10.0-windows/ChessGui.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChessGui")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6639efa87b6e8b64924d30f28fa7eb7a0d4fdd50")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChessGui")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChessGui")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
